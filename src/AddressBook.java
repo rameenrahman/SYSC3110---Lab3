4ada8c40dc881095d6af8c9f08c326e586e52808
Rameen Rahman <rameenrahman05@gmail.com>
2025-09-17 15:58:45 -0400

    main method added with printing instructions

---
+++ src/AddressBook.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> Buddies;
+
+    public void addBuddy(BuddyInfo buddy){
+
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
